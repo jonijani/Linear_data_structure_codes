4b1a81b30511567ae1139770eb6abc28a70a1e89
jonijani <junaidzaib.web@gmail.com>
2023-06-20 23:33:43 +0100

    Created function print_all

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -13,6 +13,15 @@ class SinglyLL:
         else:
             return False
         
+    def print_ll(self):
+        if self.is_empty():
+            print("list is Empty")
+            return
+        temp = self.head
+        while temp != None:
+            print(temp.value)
+            temp = temp.next
+        
 if __name__ == "__main__":
     ll = SinglyLL()
         
