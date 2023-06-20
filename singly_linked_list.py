56994afe6235fb714a3a6992e0fde0cfe143fb84
jonijani <junaidzaib.web@gmail.com>
2023-06-20 23:43:45 +0100

    created function add_new_node_at_end and assinged nodes to end of th linked list

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -20,6 +20,16 @@ class SinglyLL:
             return
         new_node.next = self.head
         self.head = new_node
+
+    
+    def add_new_node_at_end(self, new_node):
+        if self.is_empty():
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next != None:
+            temp = temp.next
+        temp.next = new_node
         
 
 
@@ -35,17 +45,23 @@ class SinglyLL:
 if __name__ == "__main__":
     ll = SinglyLL()
 
-    node1 = Node(2)
+    node1 = Node(1)
     node2 = Node(2)
     node3 = Node(3)
-    node4 = Node(2)
-    node5 = Node(2)
-    node6 = Node(4)
+    node4 = Node(4)
+    node5 = Node(5)
+    node6 = Node(6)
     node7 = Node("node7")
 
-    ll.Add_new_node_at_start(node1)
-    ll.Add_new_node_at_start(node2)
-    ll.Add_new_node_at_start(node3)
+    # ll.Add_new_node_at_start(node1)
+    # ll.Add_new_node_at_start(node2)
+    # ll.Add_new_node_at_start(node3)
+
+    ll.add_new_node_at_end(node1)
+    ll.add_new_node_at_end(node2)
+    ll.add_new_node_at_end(node3)
+    ll.add_new_node_at_end(node4)
+    ll.add_new_node_at_end(node5)
 
     ll.print_ll()
         
