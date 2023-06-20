4fb303151be23d77d3218a803ed5a070a86a8622
jonijani <junaidzaib.web@gmail.com>
2023-06-21 00:02:56 +0100

    created class for CircularSinglyLinkedList

--- circular_singly_linked_list.py
+++ circular_singly_linked_list.py
@@ -3,4 +3,14 @@ class Node:
         self.value = value
         self.next = None
 
-        
+
+
+class CircularSinglyLinkedList:
+  def __init__(self):
+    self.head = None
+
+  def is_empty(self):
+    if self.head == None:
+      return True
+    else:
+      return False
