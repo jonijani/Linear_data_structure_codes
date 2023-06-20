e26ae7d3ddb81c6efb96958b3751ced0b9dc7542
jonijani <junaidzaib.web@gmail.com>
2023-06-20 23:28:19 +0100

    Singly linked list class created

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -2,3 +2,16 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+class SinglyLL:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        if self.head == None:
+            return True
+        else:
+            return False
+        
+
+        
