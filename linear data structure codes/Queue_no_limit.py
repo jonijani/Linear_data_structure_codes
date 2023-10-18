0f8e24a5af3b7c4ddc4845b7df61675b87d4a8b4
jonijani <junaidzaib.web@gmail.com>
2023-10-18 18:47:04 +0100

    created function is_empty

--- linear data structure codes/Queue_no_limit.py
+++ linear data structure codes/Queue_no_limit.py
@@ -2,6 +2,21 @@ class Queue:
     def __init__(self):
         self.elements = []
 
+      # is empty
+    def is_empty(self):
+        if self.elements == []:
+            return True
+        else:
+            return False
+
+
+
+
+
+
+
+
+
 
 
 
