28bdd161194e5910cbbeb357eb123b3ae0cf11d2
jonijani <junaidzaib.web@gmail.com>
2023-12-22 07:55:16 +0000

    created linked in class

--- linear data structure codes/queue_with_linked_list.py
+++ linear data structure codes/queue_with_linked_list.py
@@ -2,3 +2,9 @@ class Node:
   def __init__(self,value):
       self.value = value
       self.next = None
+
+
+class linkedList:
+  def __init__(self):
+      self.head = None
+      self.tail = None
